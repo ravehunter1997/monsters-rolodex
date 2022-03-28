9a008446268622d5db3e18907d54acd1894537b4
Anuj Verma <anujhim97@gmail.com>
2022-03-28 14:35:30 +0530

    Resetting back to ormal

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ useEffect(()=>{
         
 return(
   <div className="App">
-     <h1 className='app-title'>Monsters Rolodex</h1>
+     <h1 className='app-title'>Monsters Rol0odex</h1>
      <SearchBox
        className = 'search-box'
        placeholder = 'Search Monsters' 
